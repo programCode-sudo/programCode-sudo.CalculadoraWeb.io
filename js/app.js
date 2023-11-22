aaa58297a7efbde7a2c59e9a16295df08460a13e
programCodesudo <brandonjose2001@hotmail.com>
2023-11-21 20:38:06 -0600

    cambiosSonidos

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 let pantalla = "";
+var popSound = new Audio('http://codeskulptor-demos.commondatastorage.googleapis.com/pang/pop.mp3');
+var tapSound = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/click.mp3');
+var equalSound = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/clickUp.mp3')
+var errorSound = new Audio('http://codeskulptor-demos.commondatastorage.googleapis.com/descent/spring.mp3');
+var deleteAllSound = new Audio('https://rpg.hamsterrepublic.com/wiki-images/d/db/Crush8-Bit.ogg');
+
 
 function Agregar(text) {
   switch (text) {
     case "C":
       pantalla = "";
       document.getElementById("pantalla").value=pantalla;
+      deleteAllSound.play();
       break;
     default:
       pantalla += text;
       document.getElementById("pantalla").value = pantalla;
+      tapSound.play();
   }
 }
 
@@ -18,8 +26,10 @@ function Calcular(){
         let resultado = eval(pantalla);
         pantalla = resultado.toString();
         document.getElementById("pantalla").value = pantalla;
+        equalSound.play();
     } catch (error) {
         // Si hay un error, muestra "Log ERR"
+        errorSound.play();
         pantalla="Log ERR";
         document.getElementById("pantalla").value = pantalla;
         pantalla="";
@@ -27,6 +37,9 @@ function Calcular(){
 }
 
 function Borrar(){
+   popSound.play();
    pantalla=pantalla.slice(0,-1);
    document.getElementById("pantalla").value = pantalla;
 }
+
+
